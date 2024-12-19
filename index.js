a40f2f5f3dfd846590b0d34fcc2ad71c5dc15496
naitsircnat <cristiangerardchen@gmail.com>
2024-12-19 07:06:56 +0000

    Added connection to mysql

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hbs = require("hbs");
 const wax = require("wax-on");
 require("dotenv").config();
+const { createConnection } = require("mysql2/promise");
 
 let app = express();
 app.set("view engine", "hbs");
@@ -12,15 +13,27 @@ wax.on(hbs.handlebars);
 wax.setLayoutPath("./views/layouts");
 
 const helpers = require("handlebars-helpers");
+
 helpers({
   handlebars: hbs.handlebars,
 });
 
-const helpers = require("handlebars-helpers");
+let connection;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+async function main() {
+  connection = await createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+  });
+
+  app.get("/", (req, res) => {
+    res.send("Hello, World!");
+  });
+}
+
+main();
 
 app.listen(3000, () => {
   console.log("Server started");
